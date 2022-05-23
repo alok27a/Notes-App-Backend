6a49c5d79808a2a77135f22b18d2cc5830377ee8
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2022-05-23 17:12:54 +0530

    Models schema setup

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const connectToMongo = require('./DB');
+const express = require('express')
 
 connectToMongo();
+const app = express()
+const port = 3000
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
