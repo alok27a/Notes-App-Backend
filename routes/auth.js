1c7137a78e084fe2c2d7fc4385c04299f1ed734e
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2022-05-24 12:18:06 +0530

    hashing of password done using bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,10 @@ const res = require('express/lib/response');
 const router = express.Router();
 const User = require("../models/User")
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+// JSON Web Token used for verifying a user at every stage 
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'aloksec$et';
 
 // POST Endpoint to /api/auth/createnewuser
 router.post('/createnewuser',
@@ -28,20 +32,35 @@ router.post('/createnewuser',
                 return res.status(400).json({ error: "User already exists" })
             }
 
+            //  Hashing password using bcrypt
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
+
             // Creating a new user   
             user = await User.create({
                 name: req.body.name,
-                password: req.body.password,
+                password: secPass,
                 email: req.body.email,
             })
-            
+
+            // Creating auth token
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_SECRET);
+
             // Sending response that user has been saved successfully 
-            res.json({ "user status": "Saved" })
+            res.json({ authToken })
 
         } catch (error) {
-            res.status(500).json({message: error.message})
+            res.status(500).json({ message: error.message })
         }
 
     })
 
+
+
+    
 module.exports = router
