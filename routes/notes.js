3542b5611d2878a88305abbc93d2d799dd92bf00
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2022-05-25 12:27:43 +0530

    Updation of notes

--- routes/notes.js
+++ routes/notes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const fetchuser = require('../middleware/fetchUser')
 const Note = require("../models/Note")
 const { body, validationResult } = require('express-validator');
+const { findByIdAndUpdate } = require('../models/Note');
 
 
 // ROUTE 1 : to fetch all notes 
@@ -44,6 +45,35 @@ router.post('/addnote', fetchuser,
     })
 
 // ROUTE 3 : Update all notes 
+router.put('/updatenote/:id', fetchuser,
+    async (req, res) => {
+        const { title, description, tag } = req.body;
+        // Create a newnote object
+        const newNote = {};
+        if (title) {
+            newNote.title = title;
+        }
+        if (description) {
+            newNote.description = description;
+        }
+        if (tag) {
+            newNote.tag = tag;
+        }
+
+        //  Find the note and update it 
+        // But first we need to verify whether its the same user note or not
+        let note = await Note.findById(req.params.id)
+        if (!note) {
+            return res.status(404).send("Not found");
+        }
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Unauthorised access");
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote},{new: true})
+        res.json({note})
+
+    })
 
 
 module.exports = router
