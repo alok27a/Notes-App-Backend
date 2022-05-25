85cbf5af79f7cd332c2994c8d441846d36b36d34
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2022-05-25 11:38:30 +0530

    Storing anf fetch all notes

--- models/Note.js
+++ models/Note.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const NotesSchema = mongoose.Schema({
+      // this is like foreign key to identify notes of a particular user
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     title: {
         type: String,
         required: true
--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,49 @@
 const express = require('express');
 const res = require('express/lib/response');
 const router = express.Router();
+const fetchuser = require('../middleware/fetchUser')
+const Note = require("../models/Note")
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req,res)=>{
-    
-    res.json([])
-})
+
+// ROUTE 1 : to fetch all notes 
+router.get('/fetchallnotes', fetchuser,
+    async (req, res) => {
+        const notes = await Note.find({ user: req.user.id })
+        res.json(notes)
+    })
+
+
+// ROUTE 2 : to store all notes 
+router.post('/addnote', fetchuser,
+    [
+        body('title', 'Title cannot be blank').isLength({ min: 3 }),
+        body('description', 'Description cannot be blank').isLength({ min: 3 })
+    ],
+    async (req, res) => {
+
+        try {
+            const { title, description, tag } = req.body;
+
+            // If there are errors return error with its message
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                console.log(errors.message)
+                return res.status(400).json({ errors: errors.array() });
+            }
+            const note = new Note({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save();
+            res.json(savedNote)
+
+        } catch (error) {
+            console.log(error.message)
+            res.status(500).json({ message: error.message })
+        }
+    })
+
+// ROUTE 3 : Update all notes 
 
 
 module.exports = router
